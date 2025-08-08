ad4a6e4f99620dd016af3c815ecb3ddf00e0183f
Aitor Garcia Diez <bbminze@gmail.com>
2025-08-08 19:57:13 +0200

    webpage

--- src/app/compendium/models/translator.ts
+++
@@ -1,6 +0,0 @@
-import Translations from '../data/translations.json';
-
-export function translateComp(dict: string[], lang: string): string {
-  const i = Translations.Languages.Languages.indexOf(lang);
-  return dict[-1 < i && i < dict.length ? i : 0];
-}
