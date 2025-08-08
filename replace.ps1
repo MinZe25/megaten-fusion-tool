1b1a2b21a5220398ae083dda489e912a291dadf9
Aitor Garcia Diez <bbminze@gmail.com>
2025-08-08 20:14:59 +0200

    webpage

--- replace.ps1
+++ replace.ps1
@@ -4,8 +4,8 @@ Get-ChildItem -Path "./*.js" -File | ForEach-Object {
     $content = Get-Content $_.FullName -Raw
 
     # Perform the replacements
-    $content = $content -replace 'assets\/images\/', '/megaten-fusion-tool/assets/images/'
-    $content = $content -replace 'assets\/js\/', '/megaten-fusion-tool/assets/js/'
+    $content = $content -replace  '/megaten-fusion-tool/assets/images/', 'assets/images/'
+    $content = $content -replace  '/megaten-fusion-tool/assets/images/', 'assets/js/'
 
     # Write the modified content back to the file
     $content | Set-Content $_.FullName -NoNewline
